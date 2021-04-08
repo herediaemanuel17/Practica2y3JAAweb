fdf31de3aed135c456dc26bd0404c0c8d9bed25f
Emanuel <herediaemanuel17@gmail.com>
2021-04-07 21:46:52 -0300

    Practica 2/3, login.html, servlets LoginUsr y Productos, funcionando, falta implementar el facturar y crear la lista con las golosinas, precios y respectivas cantidades ingresadas en servlet Productos

--- Practica2/src/main/java/misservlets/LoginUsr.java
+++ Practica2/src/main/java/misservlets/LoginUsr.java
@@ -1,7 +1,9 @@
 package misservlets;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -10,15 +12,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
+import jakarta.servlet.annotation.WebInitParam;
 /**
  * Servlet implementation class LoginUsr
  */
-@WebServlet("/LoginUsr")
+
+@WebServlet( urlPatterns = {"/LoginUsr"},
+initParams = {
+@WebInitParam(name="usuario1", value="1234"),
+@WebInitParam(name="usuario2", value="admin")
+})
+
 public class LoginUsr extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	Hashtable logins=new Hashtable();
+	//Hashtable logins=new Hashtable();
+	public Map<String, String> logins= new HashMap<String, String>();
+	
+	
+
 
 
  
@@ -28,15 +40,25 @@ public class LoginUsr extends HttpServlet {
 
 
 	public void init(ServletConfig config) throws ServletException {
+		
 		logins.put("usuario1", config.getInitParameter("usuario1"));
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(logins.get("usuario1")!=null) {
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
+		System.out.println(logins);
+		
+		if((logins.containsKey(user)) && (logins.get(user).equals(password))) {
 			//redireccionar al servelet productos  
-			response.sendRedirect("/compras/productos");
+			System.out.println("Autenticacion correcta");
+			response.sendRedirect("Productos");
 		}else {
 			//redireccionar de nuevo a login.html
+			System.out.println("Fallo la auteticacion");
+			response.sendRedirect("/Practica2");
 		}
 	}
 
--- Practica2/src/main/java/misservlets/Productos.java
+++ Practica2/src/main/java/misservlets/Productos.java
@@ -2,68 +2,103 @@ package misservlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Productos
  */
-@WebServlet("/Productos")
+//@WebServlet("/Productos")
+
+@WebServlet( urlPatterns = {"/Productos"},
+initParams = {
+@WebInitParam(name="chupetin", value="2"),
+@WebInitParam(name="chocolate", value="2.3")
+})
 public class Productos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
+	private Map<String, String> golosinas= new HashMap<String, String>();
+	
     public Productos() {
         // TODO Auto-generated constructor stub
     }
-
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    public void init(ServletConfig config) throws ServletException {
 		
+		golosinas.put("chupetin", config.getInitParameter("chupetin"));
+		golosinas.put("chocolate", config.getInitParameter("chocolate"));
+		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Estoy en Productos y doGet");
 		response.setContentType("text/html");
 		
 		PrintWriter out=response.getWriter();
+		HttpSession sesion = request.getSession(true);
+		
 		out.println("<HTML>");
 		out.println("<HEAD>");
-		out.println("<TITLE> Hola Mundo </TITLE>");
+		out.println("<TITLE> Productos  </TITLE>");
 		out.println("</HEAD>");
+		
 		out.println("<BODY>");
-		out.println("<table class='default'>");
+		out.println("<FORM action='/Practica2/Facturar' method='get'>");
+		//out.println("<table class='default' border='1'>");
+		out.println("<table border='1'>");
 		
 		out.println("<tr>");
-		out.println("<th> Golosinas </th>");
-		out.println("<th> Precio Unitario </th>");
-		out.println("<th> Cantidad </th>");
+		out.println("<th  align='center'> Golosinas   </th>");
+		out.println("<th align='center'> Precio Unitario   </th>");
+		out.println("<th align='center'> Cantidad   </th>");
 		out.println("</tr>");
 		
 		out.println("<tr>");
 		
-		out.println("<td>");
-		//Nombre de Golosinas
-		out.println("</td>");
 		
-		out.println("<td>");
-		//Precio de las golosinas
-		out.println("</td>");
+		for (String i : golosinas.keySet()) {
+			out.println("<tr align='center'>");
+				  System.out.println(i);
+				  out.println("<td align='center'>");
+				  out.print("<P>"+i+ "</P>");
+				  out.println("</td>");
+				  
+				  System.out.println(golosinas.get(i));
+				  out.println("<td align='center'>");
+					//Precio de las golosinas
+				  out.print("<P>$ "+golosinas.get(i)+ "</P>");
+				  out.println("</td>");
+					
+				  out.println("<td >");
+					//Cantidad editable por el usuario
+				  out.println("<input type='number' name='cantidad'>");
+				  out.println("</td>");
+			  out.println("</tr>");
+			}
 		
-		out.println("<td>");
-		//Cantidad editable por el usuario
-		out.println("<input type='text' name='cantidad'>");
-		out.println("</td>");
+		out.println("</TABLE>");
 		
-		out.println("</tr>");
-		
-		out.println("</table>");
 		out.println("<input type='submit' value='Facturar'>");
+		
+		out.println("</FORM>");
 		out.println("</BODY>");
 		out.println("</HTML>");
 		out.close();
+		
 	}
 
 }
